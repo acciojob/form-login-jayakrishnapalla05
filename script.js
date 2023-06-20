f32fad2c820c9e6a442c6f26ad75dd00292d332d
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-20 14:24:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getFormvalue() {
     //Write your code here
-	const first=document.querySelector("input[type=fname]").value
-    const second=document.querySelector("input[type=lname]").value;
+	const first=document.querySelector("input[name=fname]").value
+    const second=document.querySelector("input[name=lname]").value;
 	
 	alert(first +" "+second); 
 }
